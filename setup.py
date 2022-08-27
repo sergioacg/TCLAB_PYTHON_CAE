ebe972f4f2c98c889481c4c95ff99695cde2e047
Sergio Andres Castaño Giraldo <49730507+sergioacg@users.noreply.github.com>
2022-08-27 13:16:57 -0800

    Prueba de Distribucion

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ here = path.abspath(path.dirname(__file__))
 exec(open("tclab_cae/version.py").read())
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
